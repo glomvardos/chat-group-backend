01a7f5f1bcbbb004e7b3025dfbc229983f458cb5
glomvardos <glomvardos@gmail.com>
2022-06-15 00:33:01 +0300

    initial-commit

---
+++ src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { CategoryModule } from './category/category.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
+    PrismaModule,
+    CategoryModule,
+  ],
+})
+export class AppModule {}
